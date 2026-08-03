1de921160fa4b6455e7562c0cf8e3604f4bfaacb
Ezrasanga <ezrasanga013@gmail.com>
2026-08-03 15:43:14 +0300

    Hello world

---
+++ househunt-kenya/src/components/property/PropertyGrid.jsx
@@ -0,0 +1,1 @@
+export default function PropertyGrid() { return null; }
---
+++ househunt-kenya/src/config/appConfig.js
@@ -0,0 +1,7 @@
+export const APP_NAME = "HouseHunt Kenya";
+
+export const APP_VERSION = "1.0.0";
+
+export const SUPPORT_EMAIL = "support@househunt.co.ke";
+
+export const DEFAULT_CURRENCY = "KES";
